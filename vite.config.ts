58dea32e257ae726ec5fd5ef4cd71d42e86893ae
ryuujo1573 <me@ryuujo.com>
2023-02-15 20:25:20 +0800

    chore(vite): add a plugin to force full reloads

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import React from '@vitejs/plugin-react-swc'
+import FullReload from 'vite-plugin-full-reload'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react({
+    React({
       tsDecorators: true,
     }),
+    FullReload(['src/**/*']),
   ],
 })
