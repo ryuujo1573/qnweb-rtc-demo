43bcb507eea264e91db7af67c8810461b6d06892
ryuujo1573 <me@ryuujo.com>
2023-05-25 16:48:04 +0800

    chore: use tls for LAN debugging

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import fullReload from 'vite-plugin-full-reload'
 import { visualizer } from 'rollup-plugin-visualizer'
+import fs from 'node:fs'
 // import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
@@ -29,7 +30,17 @@ export default defineConfig({
     },
   },
   server: {
-    host: '0.0.0.0',
-    // https: true,
+    host: true,
+    port: 5173,
+    strictPort: true,
+  },
+  preview: {
+    host: true,
+    port: 443, // use: https://localhost/
+    strictPort: true,
+    https: {
+      key: fs.readFileSync('cert/server.key'),
+      cert: fs.readFileSync('cert/server.crt'),
+    },
   },
 })
