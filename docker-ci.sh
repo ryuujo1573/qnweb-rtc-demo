4964448eff2dec200cf66f2075eacd334134d4d1
ryuujo1573 <me@ryuujo.com>
2023-07-11 17:40:35 +0800

    ci: yolo

--- docker-ci.sh
+++ docker-ci.sh
@@ -11,15 +11,15 @@ const dep = demo.dependencies['qnweb-rtc']
 if (dep.startsWith('link:')) {
   const pkg = require(dep.slice(5) + '/package.json')
   console.log(pkg.version, demo.version)
-  // process.exit(pkg.version[0])
 } else {
-  // process.exit(1)
+  console.log(dep, demo.version)
 }
 JS
 )
 # if sdk version is empty, exit
 if [[ -n "$SDK_VERSION" ]]; then
-  echo SDK Version: $SDK_VERSION, Demo Version: $DEMO_VERSION
+  echo "qnweb-rtc@$SDK_VERSION 
+  demo@$DEMO_VERSION" | column -t -s@
 else
   exit 6
 fi
@@ -40,6 +40,34 @@ cfg
 fi
 set +a
 
+if (! docker stats --no-stream &>/dev/null); then
+  case "$OSTYPE" in
+  darwin*)
+    open /Applications/Docker.app
+    ;;
+  linux*)
+    sudo systemctl start docker
+    ;;
+  esac
+  # anim3=(⠋ ⠇ ⠦ ⠴ ⠸ ⠙)
+  # anim2=(⠉ ⠃ ⠆ ⠤ ⠰ ⠘)
+  anim=(⠋ ⠙ ⠹ ⠸ ⠼ ⠴ ⠦ ⠧ ⠇ ⠏)
+  dots=('.  ' '.. ' '...' '   ')
+  i=1
+  while (! docker stats --no-stream &>/dev/null); do
+    t=0
+    # Docker takes a few seconds to initialize
+    [[ j=$((j + 1)) -gt 3 ]] && j=0
+    for t in {1..10}; do
+      [[ i=$((i + 1)) -gt 5 ]] && i=0
+      echo -ne "\r${anim[$i]} Waiting for Docker to launch${dots[$j]} "
+      sleep 0.05
+      t=$((t + 1))
+    done
+  done
+
+fi
+
 # login to registry
 echo "$DOCKER_CREDENTIAL" | docker login --username=$DOCKER_USERNAME $DOCKER_REGISTRY --password-stdin
 
@@ -52,13 +80,6 @@ docker build -t $DOCKER_REGISTRY/$DOCKER_PATH:$SDK_VERSION . --platform linux/am
 # push docker image
 docker push $DOCKER_REGISTRY/$DOCKER_PATH:$SDK_VERSION
 
-# use ssh to connect to server host and executing commands on server.
-# first, pull the latest image on server.
-# then, query the running containers which ancestor image is the image.
-#   if there is a running one, stop it and run a new one with the same port.
-#   if there is no running one, run a new one with default port from env,
-#     which removes itself when stopped.
-# here's the script:
 ssh $SERVER_HOST <<EOF
   docker pull $DOCKER_REGISTRY/$DOCKER_PATH:$SDK_VERSION
   docker stop \$(docker ps -q --filter name=qnweb-rtc-demo)
