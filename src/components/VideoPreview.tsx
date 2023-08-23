ba58a43f70e2f101c999f8346e894be15a6ad282
ryuujo1573 <me@ryuujo.com>
2023-08-23 14:15:51 +0800

    feat: `VideoPreview`click'n play

--- src/components/VideoPreview.tsx
+++ src/components/VideoPreview.tsx
@@ -1,45 +1,47 @@
 import { Box, BoxProps, Skeleton } from '@mui/material'
-import QNRTC, { QNCameraVideoTrack, QNLocalVideoTrack } from 'qnweb-rtc'
+import QNRTC, { QNLocalVideoTrack } from 'qnweb-rtc'
 import { useEffect, useRef, useState } from 'react'
 import { useAppSelector } from '../store'
+import { PersonalVideoTwoTone } from '@mui/icons-material'
 
 export interface VideoPreviewProps {}
 
 const VideoPreview = ({ ...boxProps }: VideoPreviewProps & BoxProps) => {
   const { mirror, facingMode, defaultCamera, cameraPreset } = useAppSelector(
-    (s) => s.settings
+    (s) => s.settings,
   )
 
   const boxRef = useRef<HTMLDivElement>()
   const [track, setTrack] = useState<QNLocalVideoTrack>()
-  const showSkeleton = !track
+  const [display, setDisplay] = useState(false)
 
   useEffect(() => {
     // console.log('# create')
     let track: QNLocalVideoTrack | undefined
 
-    QNRTC.createCameraVideoTrack({
-      facingMode: undefined,
-      cameraId: defaultCamera,
-      encoderConfig: cameraPreset,
-    }).then((newTrack) => {
-      track = newTrack
-      setTrack((oldTrack) => {
-        // if track is recreated multiple times,
-        // destroy the old, and remain the new.
-        if (oldTrack) {
-          // console.log('# inner clear')
-          oldTrack.destroy()
-        }
-        return newTrack
+    if (display) {
+      QNRTC.createCameraVideoTrack({
+        facingMode: undefined,
+        cameraId: defaultCamera,
+        encoderConfig: cameraPreset,
+      }).then((newTrack) => {
+        track = newTrack
+        setTrack((oldTrack) => {
+          // if track is recreated multiple times,
+          // destroy the old, and remain the new.
+          if (oldTrack) {
+            // console.log('# inner clear')
+            oldTrack.destroy()
+          }
+          return newTrack
+        })
       })
-    })
-
-    return function cleanEffect() {
-      // console.log('# clear', track)
-      track?.destroy()
+      return function cleanEffect() {
+        // console.log('# clear', track)
+        track?.destroy()
+      }
     }
-  }, [facingMode, defaultCamera, cameraPreset])
+  }, [display, facingMode, defaultCamera, cameraPreset])
 
   useEffect(() => {
     if (boxRef.current) {
@@ -51,17 +53,52 @@ const VideoPreview = ({ ...boxProps }: VideoPreviewProps & BoxProps) => {
     <Box
       className={mirror ? 'mirror' : undefined}
       ref={boxRef}
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      bgcolor="black"
       sx={{
+        borderRadius: '18px',
         position: 'relative',
-        // maxHeight: '300px',
-        height: 'fit-content',
+        minHeight: '180px',
+        // height: 'fit-content',
       }}
       {...boxProps}
     >
-      {showSkeleton ? (
-        <Skeleton variant="rectangular" width="100%" height="100%" />
+      {display ? (
+        <>
+          <Skeleton variant="rectangular" width="100%" height="100%"></Skeleton>
+        </>
       ) : (
-        <></>
+        <Box
+          component="a"
+          // display="contents"
+          sx={{
+            position: 'relative',
+            cursor: 'pointer',
+            width: '2rem',
+            height: '2rem',
+            '&>svg': {
+              height: '100%',
+              width: '100%',
+            },
+            '::after': {
+              position: 'absolute',
+              textAlign: 'center',
+              left: '50%',
+              transform: 'translate(-50%, 0)',
+              bottom: '-1.5rem',
+              fontSize: '.8rem',
+              content: '"预览"',
+              width: 'max-content',
+            },
+          }}
+          onClick={() => {
+            setDisplay(true)
+          }}
+        >
+          <PersonalVideoTwoTone />
+        </Box>
       )}
     </Box>
   )
